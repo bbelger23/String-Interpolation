5a5aa8cee970244c4b60d74ba66dc34439fe06f4
Your Name <you@example.com>
2021-09-08 20:32:37 -0500

    Added code for date and pi

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace String_Interpolation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DateTime today = DateTime.Now;
+            var pi = Math.PI;
+
+            Console.WriteLine($"1. {today:MMMM} {today:dd}, {today:yyyy}");
+            Console.WriteLine($"2. {today:yyyy}.{today:MM}.{today:dd}");
+            Console.WriteLine($"3. Day {today:dd} of {today:MMMM}, {today:yyyy}");
+            Console.WriteLine($"4. Year:{today:yyyy},Month:{today:MM},Day:{today:dd}");
+            Console.WriteLine($"5. {today,19:dddd}");
+            Console.WriteLine($"6. {today,5:hh}:{today:mm} {today:tt} {today,19:dddd}");
+            Console.WriteLine($"7. h:{today:hh},m:{today:mm},s:{today:ss}");
+            Console.WriteLine($"8. {today:yyyy}.{today:MM}.{today:dd}.{today:hh}.{today:mm}.{today:ss}");
+
+            Console.WriteLine($"1. {pi:C}");
+            Console.WriteLine($"2. {pi,10:N3}");
         }
     }
 }
